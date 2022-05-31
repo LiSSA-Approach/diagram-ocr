a664ec3316b73cb6e938e3afa9c39c6f5252259f
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-05-31 15:44:21 +0300

    Added regions to API

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from typing import Dict, Any
+from typing import Dict, Any, List, Optional, Union
 
-from flask import Flask, Blueprint, make_response
+from flask import Flask, Blueprint, make_response, abort
 from flask_restx import Resource, Api
 
 from werkzeug.datastructures import FileStorage
 from ocr import OCR
 
-flask = Flask("MultiNLU")
+flask = Flask("DiagramOCR")
 api = Api(version="0.1", title="Diagram OCR API", default_label="Fallback API", default="Fallback")
 
 ns = api.namespace("ocr", "ocr namespace")
 
 upload_parser = api.parser()
 upload_parser.add_argument('file', location='files', type=FileStorage, required=True)
-
+upload_parser.add_argument('regions', location='args', type=str, help="(x1,y1,x2,y2) x N", required=False)
 ocr = OCR()
+
 
 @ns.route("/")
 class OCREndpoint(Resource):
@@ -23,12 +24,22 @@ class OCREndpoint(Resource):
 
     @api.expect(upload_parser)
     @api.response(200, "if recognition was successful")
-    def post(self) -> Dict[str, Any]:
+    @api.response(400, "if the parameters are not good")
+    def post(self):
         args = upload_parser.parse_args()
-        uploaded_file : FileStorage = args['file']  # This is FileStorage instance
+        uploaded_file: FileStorage = args['file']  # This is FileStorage instance
+        regions: Union[str, List[List[int]]] = args['regions']
+        if not regions:
+            regions = []
+        else:
+            ints = [int(value.strip()) for value in regions.split(",")]
+            if len(ints) % 4 != 0:
+                abort(400, "You cannot provided regions % 4 != 0")
+            regions = [ints[i:i + 4] for i in range(0, len(ints), 4)]
+
         data = uploaded_file.read()
-        result = ocr.interprete(data)
-        print(result)
+        result = ocr.interpret(data, regions)
+        # print(result)
         return result
 
     @api.response(200, f"Hello from Sketch Recognition {api.version}")
--- ocr.py
+++ ocr.py
@@ -1,18 +1,43 @@
+from typing import List
+
 import numpy as np
 import cv2
 import pytesseract
 
+
 class OCR:
     def __init__(self):
-        pass
+        self._ctr = 0
 
-    def interprete(self, data: bytes):  # -> Union[str, dict]:
+    def interpret(self, data: bytes, regions: List[List[int]]):  # -> Union[str, dict]:
         try:
             npdata = np.fromstring(data, np.uint8)
             image = cv2.imdecode(npdata, cv2.IMREAD_COLOR)
+            height, width, channels = image.shape
         except Exception as error:
             return f"Could not predict: {error}"
 
+        if len(regions) == 0:
+            regions = [[0, 0, width - 1, height - 1]]
+
+        result_data = []
+        for region in regions:
+            found_data = self._process_region(image.copy(), region, width, height)
+            result_data += found_data
+        return result_data
+
+    def _process_region(self, original_image, region, original_width, original_height):
+
+        x1, y1, x2, y2 = region[0], region[1], region[2], region[3]
+        # Ensure correct dimensions
+        x1 = max(0, x1)
+        y1 = max(0, y1)
+        x2 = min(x2, original_width - 2)
+        y2 = min(y2, original_height - 2)
+
+        image = original_image[y1:y2, x1:x2]
+        cv2.imwrite(f"./image{self._ctr}.png", image)
+        self._ctr += 1
         d = pytesseract.image_to_data(image, lang="eng", output_type=pytesseract.Output.DICT)
         n_boxes = len(d['level'])
         result_data = []
